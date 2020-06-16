bc5a33d3a00e400116e87a1820859e154089c310
agusTrih <ant3dong@gmail.com>
2020-06-16 22:24:22 +0700

    i use forEach to add list in body :)

--- index.js
+++ index.js
@@ -1,21 +1,30 @@
-const resep = document.querySelector(".resep");
+// deklarasi
 const h1 = document.getElementById("heading");
 const container = document.querySelector(".container");
-resep.innerHTML = `<ul><li>1 bungkus indomie</li>
-<li>1 buah Telur</li>
-<li>2 gelas air</li>
-<li>garam</li>
-<li>bawang</li>
-<li>bakso</li><ul`;
-const gambar = document.createElement("img");
-const src = document.createAttribute("src");
-src.value = "image.jpg";
 
-gambar.setAttributeNode(src);
-resep.appendChild(gambar);
+// add li in list
+const arrList = ["1 bungkus indomie", "telur", "bawang", "bakso", "garam"];
+const ul = document.createElement("ul");
+document.querySelector(".list").appendChild(ul);
+arrList.forEach((el) => {
+    const itemList = document.createTextNode(el);
+    const li = document.createElement("li");
+    li.appendChild(itemList);
+    ul.appendChild(li);
+});
+
+// image in image
+const image = document.querySelector(".image");
+const img = document.createElement("img");
+const src = document.createAttribute("src");
+// src join img and image
+src.value = "image.jpg";
+img.setAttributeNode(src);
+image.appendChild(img);
+
 // style
-heading.style.textAlign = "center";
-gambar.style.width = "300px";
+
+img.style.width = "300px";
 container.style.display = "flex";
 container.style.justifyContent = "center";
 container.style.flexDirection = "column";
